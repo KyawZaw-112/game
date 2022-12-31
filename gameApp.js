e7efe1faa102e9a921d5da912ce40b89999f18e0
KyawZaw-112 <winkyawzaw660@gmail.com>
2022-12-31 10:57:02 +0630

    first version

---
+++ gameApp.js
@@ -0,0 +1,23 @@
+let char = document.querySelector("#char");
+let block = document.querySelector("#block");
+
+function jump() {
+    if (char.classList != "animate") {
+        char.classList.add("animate");
+    }
+    setTimeout(function () {
+        char.classList.remove("animate")
+    },500);
+}
+
+let checkDead = setInterval(function () {
+    let charTop = parseInt(window.getComputedStyle(char).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if (blockLeft<20 && blockLeft>0 && charTop>=130) {
+        block.style.animation = "none";
+        block.style.display = "none";
+        char.style.display = "none"
+        char.style.animation = "none"
+        alert("You Lose")
+    }
+},10)
